4ae3bd601f2a686f4d463c9eb8bbff50ff0dddc1
Joao Lucas Ramos Suarez <ramossuarezjl@gmail.com>
2023-09-24 17:11:18 -0300

    Agregado CORS

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,12 +1,25 @@
-import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-import {Socket} from 'socket.io'
+import { ConnectedSocket, MessageBody, OnGatewayConnection, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {Socket, Server} from 'socket.io'
 
-@WebSocketGateway()
-export class ChatGateway {
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class ChatGateway implements OnGatewayConnection {
+
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: any, ...args: any[]) {
+    console.log('ccc');
+    
+  }
   @SubscribeMessage('sendChat')
   handleMessage(@ConnectedSocket() client: Socket, @MessageBody() chatText:string) {
-    console.log(chatText);
-    client.emit('event', chatText);
+    console.log('Message '.concat(chatText));
+    //client.emit('event', chatText);
+    this.server.emit('event', chatText)
     //return 'Hello world!';
   }
 }
