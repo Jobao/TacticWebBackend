41739e526cba720ee9a4e578be2ef63c0014223e
Joao Lucas Ramos Suarez <ramossuarezjl@gmail.com>
2023-09-29 16:44:54 -0300

    remove logs

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -49,8 +49,6 @@ export class AuthGuard implements CanActivate {
   }
 
   private extractTokenFromHeader(request: Request): string | undefined {
-    console.log(request['handshake']['headers']['authorization']);
-    
     const [type, token] = request['handshake']['headers']['authorization'].split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
   }
