0d8c4d2ab140b9bf341d9613863723e3f10e46b3
Joao Lucas Ramos Suarez <ramossuarezjl@gmail.com>
2023-10-09 15:35:11 -0300

    Instalado swagger

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthGuard } from './auth/auth.guard';
 import { JwtService } from '@nestjs/jwt';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalGuards(new AuthGuard(new JwtService(), app.get(Reflector)));
+  /*const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);*/
   await app.listen(3000);
 }
 bootstrap();
