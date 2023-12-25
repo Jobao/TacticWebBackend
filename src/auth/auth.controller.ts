7b331a4989b336a1a8a91479eda93b4b9a57d6ff
Joao Lucas <ramossuarezjl@gmail.com>
2023-12-25 19:47:35 -0300

    Ahora el login devuelve el token y el useruuid

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,32 +1,46 @@
-import { Body, Controller, Get, Post, Res, UseGuards} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  Response,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { Public } from './public.decorator';
 import { SignupDto } from './dto/signup.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthHTTPGuard } from './authHTTP.guard';
+import { Request } from 'express';
 
 @Controller('')
 @ApiTags('Auth')
 export class AuthController {
-    constructor(private authService: AuthService){}
+  constructor(private authService: AuthService) {}
 
-    @Public()
-    @Post('login')
-    async login(@Body() payload: LoginDto){
-        let v = await this.authService.login(payload);
-        
-       return v;
-    }
+  @Public()
+  @Post('login')
+  async login(@Body() payload: LoginDto, @Req() request: Request) {
+    let v = await this.authService.login(payload);
+    request.res.setHeader(
+      'Set-Cookie',
+      `token=${v.access_token}; HttpOnly; Secure`,
+    );
+    //Estoy cambiiando para que devuelva el repsonse
+    return v;
+  }
 
-    @Public()
-    @Post('signup')
-    async signup(@Body() payload: SignupDto){
-        this.authService.signup(payload);
-    }
-    @UseGuards(AuthHTTPGuard)
-    @Get('ping')
-    async ping(){
-        return {status:'ok'}
-    }
+  @Public()
+  @Post('signup')
+  async signup(@Body() payload: SignupDto) {
+    this.authService.signup(payload);
+  }
+  @UseGuards(AuthHTTPGuard)
+  @Get('ping')
+  async ping() {
+    return { status: 'ok' };
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,47 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
-import { Auth} from './auth.schema';
+import { Auth } from './auth.schema';
 import { LoginDto } from './dto/login.dto';
 import { v4 as uuidv4 } from 'uuid';
 import { SignupDto } from './dto/signup.dto';
 import { MongodbService } from 'src/mongodb/mongodb.service';
+import { Request } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-    constructor(private jwtService: JwtService, private userService:UserService,private mongoService:MongodbService) {}
+  constructor(
+    private jwtService: JwtService,
+    private userService: UserService,
+    private mongoService: MongodbService,
+  ) {}
 
-    async login(payload: LoginDto){
-        let r= await this.mongoService.authRepository.findOne(payload.user);
-        
-        if(r){
-            if(await r.checkPassword(payload.pass)){
-                
-                const paylo = { sub: r.uuid, username: r._id };
-                return {
-                            access_token:  "Bearer " + await this.jwtService.signAsync(paylo),
-                    };
-            }
-            else{
-                return {status:"error"}
-            }
-        }
-        return {status:"error"}
+  async login(payload: LoginDto) {
+    let r = await this.mongoService.authRepository.findOne(payload.user);
+
+    if (r) {
+      let aux = '';
+      if (await r.checkPassword(payload.pass)) {
+        const paylo = { sub: r.uuid, username: r._id };
+        return {
+          access_token: 'Bearer ' + (await this.jwtService.signAsync(paylo)),
+          user_uuid: r.uuid,
+        };
+      } else {
+        return { status: 'error' };
+      }
     }
+    return { status: 'error' };
+  }
 
-    async signup(payload: SignupDto){
-        let uuid = uuidv4();
-        let auth = new Auth();
-        auth._id = payload.user;
-        auth.pass = payload.pass;
-        auth.uuid = uuid;
+  async signup(payload: SignupDto) {
+    let uuid = uuidv4();
+    let auth = new Auth();
+    auth._id = payload.user;
+    auth.pass = payload.pass;
+    auth.uuid = uuid;
 
-        try {
-            await this.mongoService.authRepository.create(auth)
-            return await this.userService.create({_id: uuid, user: payload.user, displayName: payload.displayName})
-        } catch (error) {
-            console.log(error);
-            
-        }
+    try {
+      await this.mongoService.authRepository.create(auth);
+      return await this.userService.create({
+        _id: uuid,
+        user: payload.user,
+        displayName: payload.displayName,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
