48f755a5d4d7abb37f1b65f13f0f0c504e6476c5
Joao Lucas <ramossuarezjl@gmail.com>
2024-01-22 17:55:40 -0300

    fix bugf que permitia agregar unidades sin nombre

--- src/unit/dto/createUnit.dto.ts
+++ src/unit/dto/createUnit.dto.ts
@@ -1,10 +1,13 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class CreateUnitDto{
-    @ApiProperty()
-    user_uuid:string;
-    @ApiProperty()
-    name:string;
-    @ApiProperty()
-    class_id:string;
+export class CreateUnitDto {
+  @ApiProperty()
+  user_uuid: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @ApiProperty()
+  class_id: string;
 }
